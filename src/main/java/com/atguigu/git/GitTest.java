6ac5aad9ced38cec2bff74e86511d7a5735077d7
Lin-kaifeng <95734556+Lin-kaifeng@users.noreply.github.com>
2022-03-18 14:34:10 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello,git2");
         System.out.println("hello,git4");
         System.out.println("hello,hit push");
+         System.out.println("hello,online test");
     }
 }
